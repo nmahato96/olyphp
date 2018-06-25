e731d2bec25fc64198164eef29dcc183b517a751
niladri <nmahato@aapnainfotech.in>
2018-06-25 13:44:27 +0530

    reverse back

--- header.php
+++ header.php
@@ -1,10 +1,9 @@
-			
 			<header>
 				<div class="logo col-md-1">
 					<img src="img/logo.png">
 				</div>
 				<div class="logo_text col-md-7">
-					<p>India Largest Marketplace - test</p>
+					<p>India Largest Marketplace</p>
 				</div>
 				<div class="my_account_btn col-md-2">
 					<a><i class="fas fa-user"></i> My Account</a>
