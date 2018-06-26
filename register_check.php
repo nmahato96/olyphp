00f514fb6b1c86c0aab98e1006d36f147f37a1d8
niladri <nmahato@aapnainfotech.in>
2018-06-26 12:27:43 +0530

    regular expression

--- register_check.php
+++ register_check.php
@@ -4,6 +4,11 @@
 	$password="aapna@123";
 	$dbname="olyphp";
 	
+	// $servername="localhost";
+	// $username="root";
+	// $password="";
+	// $dbname="oly";
+	
 	// Create connection
 		$conn = mysqli_connect($servername, $username, $password, $dbname);
 	
@@ -21,15 +26,21 @@
 			echo "Please fill all fields!";
 		}else{
 			if( $upass == $cpass){
-				$conv_pass = md5($upass);
-				$sql = "INSERT INTO user (name, email, pass, status)
-				VALUES ('".$uname."', '".$uemail."', '".$conv_pass."', 'false')";
+				$pattern = "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/i";
+				if( preg_match($pattern,$uemail)){
+					$conv_pass = md5($upass);
+					$sql = "INSERT INTO user (name, email, pass, status)
+					VALUES ('".$uname."', '".$uemail."', '".$conv_pass."', 'false')";
 
-				if (mysqli_query($conn, $sql)) {
-					echo "true";
-				} else {
-					echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+					if (mysqli_query($conn, $sql)) {
+						echo "true";
+					} else {
+						echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+					}
+				}else{
+					echo "invalid email";
 				}
+				
 			}else{
 				echo "Passwords don't match!";
 			}
