4200518d9ed727b0903f482023cd07301d8488b9
niladri <nmahato@aapnainfotech.in>
2018-06-26 14:46:04 +0530

    already registered check

--- register_check.php
+++ register_check.php
@@ -32,14 +32,31 @@
 					$pattern = "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/i";
 					if( preg_match($pattern,$uemail)){
 						$conv_pass = md5($upass);
-						$sql = "INSERT INTO user (name, email, pass, status)
-						VALUES ('".$uname."', '".$uemail."', '".$conv_pass."', 'false')";
+						
+						
+						
+						$sql = "SELECT * FROM user WHERE email='".$uemail."'";
+						$result = mysqli_query($conn, $sql);
 
-						if (mysqli_query($conn, $sql)) {
-							echo "true";
+						if (mysqli_num_rows($result) > 0) {
+							// output data of each row
+							while($row = mysqli_fetch_assoc($result)) {
+								if($row["status"]=="false"){
+									echo "registration incomplete. Please check mail.";
+								}else{
+									echo "Already Registered!";
+								}
+							}
 						} else {
-							echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-						}
+								$sql = "INSERT INTO user (name, email, pass, status)
+								VALUES ('".$uname."', '".$uemail."', '".$conv_pass."', 'false')";
+
+								if (mysqli_query($conn, $sql)) {
+									echo "true";
+								} else {
+									echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+								}
+							}	
 					}else{
 						echo "invalid email";
 					}
