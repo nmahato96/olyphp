f0cedb542bad2454eabed1d1453817ac29c3f11c
niladri <nmahato@aapnainfotech.in>
2018-06-26 15:54:01 +0530

    mail check

---
+++ mail.php
@@ -0,0 +1,8 @@
+<?php
+$to = "akashkgp96@gmail.com";
+$subject = "My subject";
+$txt = "Hello world!";
+$headers = "From: niladrimahato96@gmail.com" . "\r\n";
+
+mail($to,$subject,$txt,$headers);
+?> 
