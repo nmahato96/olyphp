258a6882a434da1c706cda0fe1c90b07bb9ee79c
niladri <nmahato@aapnainfotech.in>
2018-06-26 12:35:20 +0530

    pass 5 characters

--- register_check.php
+++ register_check.php
@@ -25,24 +25,28 @@
 		if( $uname == "" || $uemail == "" || $upass == ""){
 			echo "Please fill all fields!";
 		}else{
-			if( $upass == $cpass){
-				$pattern = "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/i";
-				if( preg_match($pattern,$uemail)){
-					$conv_pass = md5($upass);
-					$sql = "INSERT INTO user (name, email, pass, status)
-					VALUES ('".$uname."', '".$uemail."', '".$conv_pass."', 'false')";
-
-					if (mysqli_query($conn, $sql)) {
-						echo "true";
-					} else {
-						echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-					}
-				}else{
-					echo "invalid email";
-				}
-				
+			if(strlen($upass)<5){
+				echo "Password must contain atleast 5 characters.";
 			}else{
-				echo "Passwords don't match!";
+				if( $upass == $cpass){
+					$pattern = "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/i";
+					if( preg_match($pattern,$uemail)){
+						$conv_pass = md5($upass);
+						$sql = "INSERT INTO user (name, email, pass, status)
+						VALUES ('".$uname."', '".$uemail."', '".$conv_pass."', 'false')";
+
+						if (mysqli_query($conn, $sql)) {
+							echo "true";
+						} else {
+							echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+						}
+					}else{
+						echo "invalid email";
+					}
+					
+				}else{
+					echo "Passwords don't match!";
+				}
 			}
 		}
 
